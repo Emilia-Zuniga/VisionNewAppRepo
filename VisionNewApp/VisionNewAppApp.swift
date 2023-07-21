173e392a4b389221c9644d8d88756c1a148a5643
Emilia Zuñiga Losada <emiliazlosada14@outlook.com>
2023-07-20 18:33:46 -0600

    Initial Commit

---
+++ VisionNewApp/VisionNewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionNewAppApp.swift
+//  VisionNewApp
+//
+//  Created by Emilia Zuñiga Losada on 20/07/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionNewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
